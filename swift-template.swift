5f97bae20ab99443ad8958e42eb640e05b4adbc8
cxs457 - Christian Alexsander Santos <Christian.Santos@sherwin.com>
2025-02-20 16:19:22 -0300

    swift tempalte

---
+++ swift-template.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+import Foundation
+import PackageDescription
+
+let package = Package(
+   name: projectName,
+   platforms: [.iOS(iosVersion)],
+   products: [
+      .library(
+        name: projectName,
+        targets: [projectName]
+      ),
+   ],
+   dependencies: [],
+   targets: [
+      .binaryTarget(
+         name: projectName,
+         url: artifactUrl,
+         checksum: checksumValue
+      ),
+   ]
+)
